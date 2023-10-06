72ca362415707cac831dff6a50514be97641ea72
Tomasz <33275334+srebrzasty@users.noreply.github.com>
2023-10-06 23:36:35 +0200

    Update make_bricks.java

--- make_bricks.java
+++ make_bricks.java
@@ -2,13 +2,13 @@ public private void main(String[] args)
 {
     public boolean makeBricks(int small, int big, int goal) {
         
-        int how_many_bbricks = goal/5;
-        int how_many_sbricks = goal%5;
+        int howManyBigBricks = goal/5;
+        int howManySmallBricks = goal%5;
   
         if(small + 5*big == goal)
             return true;
   
-        if(how_many_bbricks <= big && small >= how_many_sbricks)
+        if(howManyBigBricks <= big && small >= howManySmallBricks)
             return true;
   
         if(Math.abs(goal-5*big) < small)
